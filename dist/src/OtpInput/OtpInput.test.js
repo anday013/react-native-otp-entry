94e32e3e1dc34c5975acbe81464eb1a1e7b8e796
anday013 <anday.ismayilzada@gmail.com>
2023-11-06 16:53:01 +0400

    fix: make changes in ts files

--- dist/src/OtpInput/OtpInput.test.js
+++ dist/src/OtpInput/OtpInput.test.js
@@ -25,10 +25,11 @@ describe("OtpInput", () => {
         });
         test("activePinCodeContainerStyle should not be overridden by theme", () => {
             renderOtpInput({
-                theme: { activePinCodeContainerStyle: { borderColor: "#fff" }, },
+                focusColor: "#000",
+                theme: { activePinCodeContainerStyle: { borderColor: "#fff", backgroundColor: "green" } },
             });
             const inputs = react_native_1.screen.getAllByTestId("otp-input");
-            expect(inputs[0]).toHaveStyle({ borderColor: "#000" });
+            expect(inputs[0]).toHaveStyle({ borderColor: "#fff", backgroundColor: "green" });
         });
         // Test if the number of rendered inputs is equal to the number of digits
         test.each([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])("should render the correct number of inputs: %i", (numberOfDigits) => {
